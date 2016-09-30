dc909f9c5a68cc6c48dbb973e50846a66bf0caf4
Nagireddy <Ynagireddy4u@gmail.com>
2016-09-30 07:41:42 +0530

    HelloWorld class added objecr reference

--- helloworld/src/main/java/com/example/App.java
+++ helloworld/src/main/java/com/example/App.java
@@ -9,5 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Lasses World!" + "From GitExample" );
+        
+        App app = new App();
+        app.greetMessage("Java8_Ex");
+    }
+    
+    
+    public String greetMessage(String name){
+    	return "Hello" + name;
     }
 }
